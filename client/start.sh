281e582a39343a47f82d56182204927ee367d10b
Taylor Dahlin <9667869+tfdahlin@users.noreply.github.com>
2019-12-13 13:41:42 -0500

    Reverted changes to start script.

--- client/start.sh
+++ client/start.sh
@@ -8,4 +8,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Run the op, and redirect output to an error file.
 # Simultaneously, tail the app log, and the error file to stdout
-(. $DIR/env/bin/activate && python $DIR/main.py $API_URL $OP_NAME >> $DIR/error.log) & (tail -f $DIR/app.log >&1) & (tail -f $DIR/error.log >&1)
+. $DIR/env/bin/activate && python $DIR/main.py $API_URL $OP_NAME >> $DIR/error.log
