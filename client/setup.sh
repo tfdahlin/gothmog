557ec87032a5e7cbbbb9bb84f15c5bb9debf0013
Taylor Dahlin <9667869+tfdahlin@users.noreply.github.com>
2019-12-13 10:13:20 -0500

    Updated system for containerizing client, so that docker will output the app log and error log to the terminal.

--- client/setup.sh
+++ client/setup.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+# Create the applications virtual environment
 python3 -m venv --prompt c2client env
+# Install necessary libraries
 . env/bin/activate && pip install -r requirements.txt
