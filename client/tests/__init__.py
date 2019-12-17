920c9f2b2fbe4138a69464ff7d42c2c3238446a5
Taylor Dahlin <9667869+tfdahlin@users.noreply.github.com>
2019-12-17 14:18:20 -0500

    Client test skeleton files.

---
+++ client/tests/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Filename: tests/__init__.py
+
+# Native python imports
+
+# PIP library imports
+
+# Local file imports
---
+++ client/tests/test_all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Filename: tests/test_all.py
+
+# Native python imports
+
+# PIP library imports
+
+# Local file imports
