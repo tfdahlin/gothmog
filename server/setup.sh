3a8b2e4deec1da3451e1b861c543d3a35e2ad554
Taylor Dahlin <9667869+tfdahlin@users.noreply.github.com>
2019-12-19 10:08:43 -0500

    Added a run of main to the setup script to setup the database.

--- server/setup.sh
+++ server/setup.sh
@@ -4,3 +4,4 @@ python3 -m venv --prompt c2server env
 [ ! -d "./apps/files/uploaded_files" ] && mkdir ./apps/files/uploaded_files
 . ./env/bin/activate && pip install --upgrade pip
 . ./env/bin/activate && pip install -r requirements.txt
+. ./env/bin/activate && python ./main.py
