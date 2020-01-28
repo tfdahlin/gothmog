f23ba6ceaa537917331b14d14b71f28ef8ba16a7
Taylor Dahlin <9667869+tfdahlin@users.noreply.github.com>
2020-01-28 14:35:28 -0500

    Some additional restructuring I missed in the previous commit.

--- client/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Filename: client/tests/__init__.py
-
-# Native python imports
-
-# PIP library imports
-
-# Local file imports
--- client/tests/test_all.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Filename: client/tests/test_all.py
-
-# Native python imports
-
-# PIP library imports
-
-# Local file imports
