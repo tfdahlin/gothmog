901735525920e2431ea1af18dff18ab0e3617c58
Taylor Dahlin <9667869+tfdahlin@users.noreply.github.com>
2020-01-28 14:43:19 -0500

    Slight change to client start script to improve output.

--- client/start.sh
+++ client/start.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 function usage() {
-    echo "Usage: $1 <api_url> <op_name>"
+    echo "Usage: $0 <api_url> <op_name>"
 }
 
 # Cheat to get this file's directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ -z "$API_URL" ]; then
+    usage
+    exit 1
+fi
+
+if [ -z "$OP_NAME" ]; then
+    usage
+    exit 1
+fi
 
 # Run the op, and redirect output to an error file.
 . $DIR/env/bin/activate && python $DIR/client/main.py $API_URL $OP_NAME >> $DIR/error.log
